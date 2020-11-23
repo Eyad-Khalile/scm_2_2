cfac086c09879b64540940ce2be31b0ea2c1119d
Eyad BASHA <eyad.esgi@gmail.com>
2020-11-23 02:33:33 +0100

    change email password

--- scm/settings.py
+++ scm/settings.py
@@ -214,7 +214,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'khalile.eyad@gmail.com'
-EMAIL_HOST_PASSWORD = 'Eyad1979@'
+EMAIL_HOST_PASSWORD = 'khalilmath@123'
 
 
 # KEROKU

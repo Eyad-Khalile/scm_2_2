e6e66ca567480e7df9c188062a03f3c3b6139d4c
Eyad BASHA <eyad.esgi@gmail.com>
2020-12-08 00:59:09 +0100

    setting False

--- scm/settings.py
+++ scm/settings.py
@@ -273,11 +273,11 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static/')
+# ]
 
-# STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # MEDIA
